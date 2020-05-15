96ab323f6ea44922b6226064d310a945260fcdd5
Shankar Krishnakumar <shankar.krishnakumar@kroger.com>
2020-05-15 04:55:36 -0400

    Add functionality

---
+++ src/event.ts
@@ -0,0 +1,13 @@
+import * as github from '@actions/github'
+import * as core from '@actions/core';
+export function getCreatedTag(): string | null {
+    if (github.context.eventName != 'create') {
+        core.info(`The github event was ${github.context.eventName}`)
+        return null
+    }
+    if (github.context.payload.ref_type != 'tag'){
+        core.info('The created reference was a branch, not a tag')
+        return null
+    }
+    return github.context.payload.ref
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as git from './git'
+import * as github from './github'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
-    core.setOutput('release-url', 'https://example.com')
+    var releaseUrl = ''
+    const token = core.getInput('repo-token')
+    const tag = event.getCreatedTag();
+    if (tag && version.isSemVer(tag)){
+     const changelog = await git.getChangesIntroducedByTag(tag)
+      releaseUrl = await github.createReleaseDraft(tag,token,changelog) 
+    }
+    core.setOutput('release-url', releaseUrl)
   } catch (error) {
     core.setFailed(error.message)
   }
